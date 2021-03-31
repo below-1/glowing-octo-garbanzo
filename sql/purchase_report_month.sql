b0c36916a9152e160376088efc5f1b4587c3eef6
jordan meta <jordanmeta@mail.com>
2021-03-31 18:32:11 +0800

    purchase report monthly

---
+++ sql/purchase_report_month.sql
@@ -0,0 +1,26 @@
+select 
+  o.id,
+  to_char(o.created_at, 'YYYY-MM-DD') as created_at,
+  t.code,  
+  u.first_name as "supplier",
+  u.address,
+  t.nominal as "nominal",
+  json_agg(
+    json_build_object(
+      'product', p.title,
+      'price', it.price,
+      'sale_price', it.sale_price,
+      'unit', it.quantity,
+      'available', it.available,
+      'defective', it.defective,
+      'unit', p.unit
+    ) 
+  ) as "items"
+  from "order" o
+  join "user" u on o.user_id  = u.id
+  join "transaction" t on o.transaction_id  = t.id 
+  join item it on it.order_id = o.id
+  join product p on it.product_id = p.id
+  where o."type" = 1 and o.created_at >= ? and o.created_at <= ?
+  group by o.id, u.id, t.id
+  order by o.created_at 
