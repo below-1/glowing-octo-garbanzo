716687bffc260ba2738901550fd597b80ad8f59d
jordan meta <jordanmeta@mail.com>
2021-05-29 18:12:19 +0800

    employer CRUD

--- src/handler/api/employer.ts
+++ src/handler/api/employer.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from 'fastify'
-import { QueryOrder } from '@mikro-orm/core'
+import { QueryOrder, wrap } from '@mikro-orm/core'
 import { User } from '../../entity/User'
 import { Role } from '../../entity/Role'
 import { ID } from './commons'
@@ -21,6 +21,45 @@ export default async (fastify: FastifyInstance) => {
     } catch (err) {
       console.log(err)
       reply.status(500).send({ message: 'gagal mengambil data pegawai' })
+    }
+  })
+
+  fastify.put<{ Params: ID, Body: UpdateEmployerPayload }>('/:id', {
+    handler: async (request, reply) => {
+      const em = request.em
+      const id = request.params.id
+      const payload = request.body
+      let employer = await em.findOne(User, id)
+      if (!employer) {
+        reply.status(400).send({ message: 'NOT_FOUND' })
+        return
+      }
+      employer.first_name = payload.first_name
+      employer.last_name = payload.last_name
+      employer.mobile = payload.mobile
+      employer.email = payload.email
+      em.persist(employer)
+
+      try {
+        await em.flush()
+        reply.send({ status: 'OK' })
+      } catch (err) {
+        console.log(err)
+        reply.status(500).send({ message: 'gagal mengubah data admin' })
+      }
+    }
+  })
+
+  fastify.get<{ Params: ID }>('/:id', {
+    handler: async (request, reply) => {
+      const em = request.em
+      const id = request.params.id
+      let employer = await em.findOne(User, id)
+      if (!employer) {
+        reply.status(400).send({ message: 'NOT_FOUND' })
+      } else {
+        reply.send(employer)
+      }
     }
   })
 }
